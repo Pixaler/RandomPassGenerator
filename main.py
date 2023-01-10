077a658f5b540f486017f1b69e27f63820f635c3
gnidko02@gmail.com <gnidko02@gmail.com>
2023-01-10 12:44:35 +0100

    Add more random

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+print("===================================")
+print("Welcome to the RandomPassGenerator.")
+print("===================================\n")
+nr_letters = random.randint(4, 6)
+nr_symbols = random.randint(1, 3)
+nr_numbers = random.randint(4, 6)
 
 list_password = []
 # Fill with letters
@@ -32,4 +31,4 @@ password = ""
 for char in list_password:
   password += char
 
-print(password)
+print(f"Your password is: {password}")
